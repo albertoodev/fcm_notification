cfcb95b477c60f35986595988387d10098bddee9
AMOURA Ammar abderafik <64012434+albertoodev@users.noreply.github.com>
2022-02-24 19:02:01 +0100

    test

--- README.md
+++ README.md
@@ -37,3 +37,14 @@ const like = 'sample';
 TODO: Tell users more about the package: where to find more information, how to 
 contribute to the package, how to file issues, what response they can expect 
 from the package authors, and more.
+
+add this in app\main\AndroidManifest.xml
+<meta-data
+    android:name="com.google.firebase.messaging.default_notification_channel_id"
+    android:value="channelId"
+    />
+
+ <intent-filter>
+     <action android:name="FLUTTER_NOTIFICATION_CLICK"/>
+     <category android:name="android.intent.category.DEFAULT"/>
+ </intent-filter>
--- lib/fcm_notification.dart
+++ lib/fcm_notification.dart
@@ -1,4 +1,5 @@
 library fcm_notification;
+
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -9,10 +10,15 @@ part 'local_notification/local-notification-service.dart';
 
 class FcmNotifications {
   static late final String _fcmKey;
-  static void initialize({required String fcmKey,required String channelId,required String channelName,
+
+  static void initialize({
+    required String fcmKey,
+    required String channelId,
+    required String channelName,
   }) {
     _fcmKey = fcmKey;
-    LocalNotificationsService.initialize(channelId: channelId, channelName: channelName);
+    LocalNotificationsService.initialize(
+        channelId: channelId, channelName: channelName);
   }
 
   static showNotification(RemoteMessage message) {
@@ -25,43 +31,32 @@ class FcmNotifications {
     required String body,
     required String token,
     required Map<String, dynamic> data,
-    DoneSendFunction enDone,
-    ErrorSendFunction enError,
   }) async {
-    try {
-      var response = await http.post(
-        Uri.parse('https://fcm.googleapis.com/fcm/send'),
-        headers: <String, String>{
-          'Content-Type': 'application/json',
-          'Authorization':
-          'key=$_fcmKey'
+    await http.post(
+      Uri.parse('https://fcm.googleapis.com/fcm/send'),
+      headers: <String, String>{
+        'Content-Type': 'application/json',
+        'Authorization': 'key=$_fcmKey'
+      },
+      body: jsonEncode({
+        'to': token,
+        'data': {
+          'click_action': 'FLUTTER_NOTIFICATION-CLICK',
+          ...data,
         },
-        body: jsonEncode({
-          'to':
-          token,
-          'data': {
-            'click_action': 'FLUTTER_NOTIFICATION-CLICK',
-            ...data,
-          },
-          'priority': 'high',
-          'notification': {
-            'title': title,
-            'body': body,
-          },
-        }),
-      );
-      if (response.statusCode == 200) {
-        enDone!();
-      }
-    } catch (e) {
-      enError!(e);
+        'priority': 'high',
+        'notification': {
+          'title': title,
+          'body': body,
+        },
+      }),
+    );
+  }
+
+  static _throw() {
+    if (LocalNotificationsService._channelName == null ||
+        LocalNotificationsService._channelId == null) {
+      throw ('channelName and channelId can not be null');
     }
   }
-
-
-static _throw(){
-  if(LocalNotificationsService._channelName == null || LocalNotificationsService._channelId == null){
-    throw('channelName and channelId can not be null');
-  }
-}
 }
